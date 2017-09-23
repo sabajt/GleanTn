2c81a2607a3bcc13f4d9c87b5c24526d8d240ada
John Saba <john.thomas.saba@gmail.com>
2017-09-23 16:47:09 -0500

    create profile + user data object

--- GleanTn/SignUpViewController.swift
+++ GleanTn/SignUpViewController.swift
@@ -7,46 +7,108 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+struct Profile {
+    let name: String
+    let street: String
+    let city: String
+    let state: String
+    let zip: String
+    let phone: String
+    let email: String
+    let uid: String
+    
+    var objectRep: [String: String] {
+        return [
+            "name": name,
+            "street": street,
+            "city": city,
+            "state": state,
+            "zip": zip,
+            "phone": phone,
+            "email": email
+        ]
+    }
+}
 
 class SignUpViewController: UIViewController {
 
     @IBOutlet weak var nameField: UITextField!
-    
-    @IBOutlet weak var addressField: UITextField!
-    
+    @IBOutlet weak var streetField: UITextField!
     @IBOutlet weak var cityField: UITextField!
-    
     @IBOutlet weak var stateField: UITextField!
-    
     @IBOutlet weak var zipField: UITextField!
-    
     @IBOutlet weak var phoneField: UITextField!
-    
     @IBOutlet weak var emailField: UITextField!
-    
     @IBOutlet weak var passwordField: UITextField!
     
+    var fields: [UITextField] {
+        return [nameField, streetField, cityField, stateField, zipField, phoneField, emailField, passwordField]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func createPressed(_ sender: UIButton) {
+        
+        // Make sure we have the fields
+        
+        guard let name = nameField.text, name.isEmpty == false else {
+            print("### sign up error: need name")
+            return
+        }
+        guard let street = streetField.text, street.isEmpty == false else {
+            print("### sign up error: need street")
+            return
+        }
+        guard let city = cityField.text, city.isEmpty == false else {
+            print("### sign up error: need city")
+            return
+        }
+        guard let state = stateField.text, state.isEmpty == false else {
+            print("### sign up error: need state")
+            return
+        }
+        guard let zip = zipField.text, zip.isEmpty == false else {
+            print("### sign up error: need zip")
+            return
+        }
+        guard let phone = phoneField.text, phone.isEmpty == false else {
+            print("### sign up error: need phone")
+            return
+        }
+        guard let email = emailField.text, email.isEmpty == false else {
+            print("### sign up error: need email")
+            return
+        }
+        guard let password = passwordField.text, password.isEmpty == false else {
+            print("### sign up error: need password")
+            return
+        }
+        guard let auth = FIRAuth.auth() else {
+            print("### sign up error: couldn't load firebase auth")
+            return
+        }
+        
+        // Attempt to create the Firebase user
+        
+        auth.createUser(withEmail: email, password: password) { [weak self] (usr, error) in
+            guard let user = usr, error == nil else {
+                print("### sign up error: couldn't create user: \(error!)")
+                return
+            }
+            
+            let profile = Profile(name: name, street: street, city: city, state: state, zip: zip, phone: phone, email: email, uid: user.uid)
+            self?.createProfile(profile: profile)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func createProfile(profile: Profile) {
+        let ref = FIRDatabase.database().reference(withPath: "farmers")
+        let childRef = ref.child(profile.uid)
+        childRef.setValue(profile.objectRep)
     }
-    */
-
 }
