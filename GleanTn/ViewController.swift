c1536aea20112ed900fcd8cf821a488ecce27f37
John Saba <john.thomas.saba@gmail.com>
2017-09-23 10:24:28 -0500

    Create Project

---
+++ GleanTn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GleanTn
+//
+//  Created by John Saba on 9/23/17.
+//  Copyright © 2017 Code for Nashville. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
